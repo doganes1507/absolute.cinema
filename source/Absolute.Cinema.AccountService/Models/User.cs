288ad5f9a41613dbc4970d2eae12030145c3b4ef
d.oganes1507 <d.oganes1507@gmail.com>
2025-01-31 20:18:35 +0300

    Add RegistrationDateTime property to User.
    Rename BirthDate to DateOfBirth

--- source/Absolute.Cinema.AccountService/Models/User.cs
+++ source/Absolute.Cinema.AccountService/Models/User.cs
@@ -5,6 +5,7 @@ public class User
     public Guid Id { get; set; }
     public string EmailAddress { get; set; }
     public string? FirstName { get; set; }
-    public DateOnly? BirthDate { get; set; }
+    public DateOnly? DateOfBirth { get; set; }
     public bool Gender { get; set; }
+    public DateTime RegistrationDateTime { get; set; } = DateTime.UtcNow;
 }
